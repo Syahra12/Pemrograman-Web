aed3149600869b1f959f0b0ef323f4d47b8d94e6
Syahra12 <syahraauliaputri@gmail.com>
2025-07-09 16:47:49 +0700

    new file:   Pertemuan 1/biodata diri syahra aulia.html 	new file:   Pertemuan 10/tgs10ara.css 	new file:   Pertemuan 10/tgs10ara.html 	new file:   Pertemuan 11/tgs11.css 	new file:   Pertemuan 11/tgs11.html 	new file:   Pertemuan 11/tgs11.js 	new file:   Pertemuan 12/.gitignore 	new file:   Pertemuan 12/README.md 	new file:   Pertemuan 12/package-lock.json 	new file:   Pertemuan 12/package.json 	new file:   Pertemuan 12/src/App.css 	new file:   Pertemuan 12/src/App.js 	new file:   Pertemuan 12/src/App.test.js 	new file:   Pertemuan 12/src/BookCard.js 	new file:   Pertemuan 12/src/index.css 	new file:   Pertemuan 12/src/index.js 	new file:   Pertemuan 12/src/logo.svg 	new file:   Pertemuan 12/src/public/favicon.ico 	new file:   Pertemuan 12/src/public/image/buku 2 phton.jpg 	new file:   Pertemuan 12/src/public/image/buku 3sistem_basis.jpg 	new file:   Pertemuan 12/src/public/image/buku1 menguasai_pemrograman.jpg 	new file:   Pertemuan 12/src/public/image/buku4 net.jpg 	new file:   Pertemuan 12/src/public/image/buku5.jpg 	new file:   Pertemuan 12/src/public/index.html 	new file:   Pertemuan 12/src/public/logo192.png 	new file:   Pertemuan 12/src/public/logo512.png 	new file:   Pertemuan 12/src/public/manifest.json 	new file:   Pertemuan 12/src/public/robots.txt 	new file:   Pertemuan 12/src/reportWebVitals.js 	new file:   Pertemuan 12/src/setupTests.js 	new file:   Pertemuan 2/indexara.html 	new file:   Pertemuan 3/tugaswebara (1).html 	new file:   Pertemuan 4/tugaswebara.html 	new file:   Pertemuan 5/Mesjid-RayaMedan-Sumatera-Utara-2.jpg 	new file:   Pertemuan 5/pertemuan5syahra.css 	new file:   Pertemuan 5/pertemuan5syahra.html 	new file:   Pertemuan 6/syahra.css 	new file:   Pertemuan 6/syahra.html 	new file:   Pertemuan 7/soal1.js 	new file:   Pertemuan 7/soal2.js 	new file:   Pertemuan 7/soal3.js 	new file:   Pertemuan 9/tgs9syahra.html

---
+++ Pertemuan 7/soal1.js
@@ -0,0 +1,9 @@
+// Problem 1 - String Concatenation
+let firstName = 'Syahra';
+let lastName = 'Aulia Putri';
+let bornYear = 2006;
+
+let currentYear = 2025;
+let age = currentYear - bornYear;
+
+console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.');
